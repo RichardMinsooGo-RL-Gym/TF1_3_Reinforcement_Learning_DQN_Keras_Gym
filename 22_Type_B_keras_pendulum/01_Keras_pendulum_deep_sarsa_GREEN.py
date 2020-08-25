818d80e231b9e1e27597e4bb1deb516d23fdcb1c
Richard Minsoo <67932257+RichardMinsooGo-RL-Gym@users.noreply.github.com>
2020-08-25 10:46:46 +0900

    Add files via upload

--- 22_Type_B_keras_pendulum/01_Keras_pendulum_deep_sarsa_GREEN.py
+++ 22_Type_B_keras_pendulum/01_Keras_pendulum_deep_sarsa_GREEN.py
@@ -2,7 +2,6 @@ import random
 import pylab
 import numpy as np
 import time, datetime
-from collections import deque
 import gym
 import pylab
 import sys
@@ -34,22 +33,20 @@ if not os.path.exists(model_path):
 if not os.path.exists(graph_path):
     os.makedirs(graph_path)
 
-# DQN Agent for the Cartpole
-# it uses Neural Network to approximate q function
-# and replay memory & target q network
-class DoubleDQNAgent:
+# this is DeepSARSA Agent for the Cartpole
+# Utilize Neural Network as q function approximator
+class DeepSARSAgent:
     def __init__(self):
         # if you want to see Cartpole learning, then change to True
         self.render = False
         # get size of state and action
-        self.progress = " "
         self.state_size = state_size
         self.action_size = action_size
         
         # train time define
         self.training_time = 5*60
         
-        # these is hyper parameters for the Double DQN
+        # These are hyper parameters for the DQN
         self.learning_rate = 0.001
         self.discount_factor = 0.99
         
@@ -67,19 +64,8 @@ class DoubleDQNAgent:
         
         self.ep_trial_step = 200
         
-        # Parameter for Experience Replay
-        self.size_replay_memory = 50000
-        self.batch_size = 64
-        
-        # Experience Replay 
-        self.memory = deque(maxlen=self.size_replay_memory)
-        
-        # Parameter for Target Network
-        self.target_update_cycle = 200
-
-        # create main model and target model
+        # create main model
         self.model = self.build_model()
-        self.target_model = self.build_model()
         
     # approximate Q function using Neural Network
     # state is input and Q Value of each action is output of network
@@ -93,36 +79,23 @@ class DoubleDQNAgent:
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
 
-    # pick samples randomly from replay memory (with batch_size)
-    def train_model(self):
-        # sample a minibatch to train on
-        minibatch = random.sample(self.memory, self.batch_size)
-
-        states      = np.array([batch[0] for batch in minibatch])
-        actions     = np.array([batch[1] for batch in minibatch])
-        rewards     = np.array([batch[2] for batch in minibatch])
-        next_states = np.array([batch[3] for batch in minibatch])
-        dones       = np.array([batch[4] for batch in minibatch])
-
-        states = np.squeeze(states)
-        next_states = np.squeeze(next_states)
-
-        q_value          = self.model.predict_on_batch(states)
-        q_value_next = self.model.predict_on_batch(next_states)
-        tgt_q_value_next = self.target_model.predict_on_batch(next_states)
+    # For Q-net or sarsa there is only one batch
+    def train_model(self, state, action, reward, next_state, next_action, done):
+        state = np.float32(state)
+        next_state = np.float32(next_state)
+        target = self.model.predict(state)[0]
         
-        for i in range(self.batch_size):
-            if dones[i]:
-                q_value[i][actions[i]] = rewards[i]
-            else:
-                a_max = np.argmax(tgt_q_value_next[i])
-                q_value[i][actions[i]] = rewards[i] + self.discount_factor * q_value_next[i][a_max]
-            
-        # q_update = rewards + self.discount_factor*(np.amax(tgt_q_value_next, axis=1))*(1-dones)
-        # ind = np.array([x for x in range(self.batch_size)])
-        # q_value[[ind], [actions]] = q_update
+        # like Q Learning, get maximum Q value at s'
+        # But from target model
+        if done:
+            target[action] = reward
+        else:
+            target[action] = (reward + self.discount_factor * self.model.predict(next_state)[0][next_action])
 
-        self.model.fit(states, q_value, epochs=1, verbose=0)
+        target = np.reshape(target, [1, self.action_size])
+        # make minibatch which includes target q value and predicted q value
+        # and do the model fit!
+        self.model.fit(state, target, epochs=1, verbose=0)
         
         # Decrease epsilon while training
         if self.epsilon > self.epsilon_min:
@@ -149,15 +122,6 @@ class DoubleDQNAgent:
             
         return action_arr, action
 
-    # save sample <s,a,r,s'> to the replay memory
-    def append_sample(self, state, action, reward, next_state, done):
-        #in every action put in the memory
-        self.memory.append((state, action, reward, next_state, done))
-    
-    # after some time interval update the target model to be same with model
-    def Copy_Weights(self):
-        self.target_model.set_weights(self.model.get_weights())
-
     def save_model(self):
         # Save the variables to disk.
         self.model.save_weights(model_path+"/model.h5")
@@ -169,7 +133,7 @@ class DoubleDQNAgent:
 
 def main():
     
-    agent = DoubleDQNAgent()
+    agent = DeepSARSAgent()
     
     # Initialize variables
     # Load the file if the saved file exists
@@ -195,9 +159,6 @@ def main():
     print("\n\n",game_name, "-game start at :",display_time,"\n")
     start_time = time.time()
     
-    # initialize target model
-    agent.Copy_Weights()
-    
     while time.time() - start_time < agent.training_time and avg_score < -30:
 
         state = env.reset()
@@ -207,10 +168,7 @@ def main():
         rewards = 0
         state = np.reshape(state, [1, agent.state_size])
         while not done and ep_step < agent.ep_trial_step:
-            if len(agent.memory) < agent.size_replay_memory:
-                agent.progress = "Exploration"            
-            else:
-                agent.progress = "Training"
+            # fresh env
 
             ep_step += 1
             agent.step += 1
@@ -226,38 +184,30 @@ def main():
             next_state = np.reshape(next_state, [1, agent.state_size])
             
             reward /= 10
-            rewards += reward                
-            # store the transition in memory
-            agent.append_sample(state, action, reward, next_state, done)
+            rewards += reward
+            
+            next_action_arr, next_action = agent.get_action(next_state)
+            agent.train_model(state, action, reward, next_state, next_action, done)
             
             # update the old values
             state = next_state
-            # only train if done observing
-            if agent.progress == "Training":
-                # Training!
-                agent.train_model()
-                # if done
-                if done or ep_step % agent.target_update_cycle == 0:
-                    # return# copy q_net --> target_net
-                    agent.Copy_Weights()
-
+                    
             agent.score = rewards
 
             if done or ep_step == agent.ep_trial_step:
-                if agent.progress == "Training":
-                    agent.episode += 1
-                    scores.append(agent.score)
-                    episodes.append(agent.episode)
-                    avg_score = np.mean(scores[-min(30, len(scores)):])
+                agent.episode += 1
+
+                scores.append(agent.score)
+                episodes.append(agent.episode)
+                avg_score = np.mean(scores[-min(30, len(scores)):])
                 print('episode :{:>6,d}'.format(agent.episode),'/ Rewards :{:> 4f}'.format(rewards), \
-                      '/ status :', agent.progress, \
                       '/ epsilon :{:>1.4f}'.format(agent.epsilon),'/ last 30 avg :{:> 4f}'.format(avg_score) )
                 break
     # Save model
     agent.save_model()
     
     pylab.plot(episodes, scores, 'b')
-    pylab.savefig("./save_graph/pendulum_doubledqn.png")
+    pylab.savefig("./save_graph/pendulum_deep_sarsa.png")
 
     e = int(time.time() - start_time)
     print(' Elasped time :{:02d}:{:02d}:{:02d}'.format(e // 3600, (e % 3600 // 60), e % 60))
